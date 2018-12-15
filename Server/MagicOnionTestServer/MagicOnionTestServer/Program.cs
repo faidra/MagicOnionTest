914733add7e62d50d18716992baa3b0e9be76924
faidra <faidra.windmark@gmail.com>
2018-12-15 15:05:54 +0900

    あってるか分からんけどサーバー書いた

--- Server/MagicOnionTestServer/MagicOnionTestServer/Program.cs
+++ Server/MagicOnionTestServer/MagicOnionTestServer/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MagicOnion.Server;
+using System;
+using Grpc.Core;
 
 namespace MagicOnionTestServer
 {
@@ -10,6 +8,19 @@ namespace MagicOnionTestServer
     {
         static void Main(string[] args)
         {
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+            var service = MagicOnionEngine.BuildServerServiceDefinition(true);
+            var server = new Grpc.Core.Server()
+            {
+                Services = { service },
+                Ports = { new ServerPort("localhost", 9000, ServerCredentials.Insecure) }
+            };
+
+            server.Start();
+
+            Console.WriteLine("ServerStarted");
+            Console.ReadLine();
         }
     }
 }
---
+++ Server/MagicOnionTestServer/MagicOnionTestServer/SimpleChat/SimpleChatHub.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using MagicOnion.Server.Hubs;
+
+
+namespace MagicOnionTestServer.SimpleChat
+{
+    class SimpleChatHub : StreamingHubBase<IChatRoomHub, IChatRoomHubReceiver>, IChatRoomHub
+    {
+        const string GroupIdentifier = "SimpleChatHub";
+        IGroup _room;
+        ChatRoomMember _self;
+        IInMemoryStorage<ChatRoomMember> _members;
+
+        async Task<JoinResult> IChatRoomHub.JoinAsync(string name)
+        {
+            _self = new ChatRoomMember() { Name = name };
+
+            (_room, _members) = await Group.AddAsync(GroupIdentifier, _self);
+            _self.Id = _members.AllValues.Count;
+
+            Broadcast(_room).OnJoin(_self);
+
+            return new JoinResult()
+            {
+                You = _self,
+                OtherMembers = _members.AllValues.Where(mem => mem.Id != _self.Id).ToArray()
+            };
+        }
+
+        async Task IChatRoomHub.LeaveAsync()
+        {
+            await _room.RemoveAsync(Context);
+            Broadcast(_room).OnLeave(_self.Id);
+        }
+
+        async Task IChatRoomHub.SpeakAsync(string message)
+        {
+            Broadcast(_room).OnSpeak(_self, message);
+        }
+    }
+}
